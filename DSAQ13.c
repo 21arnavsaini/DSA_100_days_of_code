d88f060bf68955547107019780309926e1d00dc7
REPOSITARY <21arnavsaini@gmail.com>
2026-03-16 15:32:35 +0530

    Q13

---
+++ DSAQ13.c
@@ -0,0 +1,47 @@
+/* You are given a rectangular matrix of integers. Starting from the outer boundary, 
+traverse the matrix in a clockwise manner and continue moving inward layer by layer until all elements are visited.*/
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[100][100];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int top = 0, bottom = r - 1, left = 0, right = c - 1;
+    int first = 1;
+
+    while (top <= bottom && left <= right) {
+        for (int j = left; j <= right; j++) {
+            if (!first) printf(" ");
+            printf("%d", a[top][j]);
+            first = 0;
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++) {
+            printf(" %d", a[i][right]);
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                printf(" %d", a[bottom][j]);
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf(" %d", a[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
